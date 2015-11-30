ec6d8817ce76f2ab497e215b1937eb461502b758
mjsiu <msiu23@gmail.com>
2015-11-29 16:47:44 -0800

    Create prototypeInheritance practices

---
+++ prototypeInheritance.js
@@ -0,0 +1,14 @@
+// 1. Declarative style constructor
+function Computer (name, company) {
+  this.name = name;
+  this.owner = owner;
+}
+// 2. Expressive style constructor
+var Model = function(modelType) {
+  this.modelType = modelType;
+}
+
+// Prototype Inheritance
+
+var gizzmo = new Animal("Gizzmo", "Ned");
+console.log(gizzmo);
