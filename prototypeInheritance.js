753e4d2fc70be846ac05dc851cf158ee6004d8f2
mjsiu <msiu23@gmail.com>
2015-11-29 17:48:16 -0800

    Finished args practice

--- prototypeInheritance.js
+++ prototypeInheritance.js
@@ -10,5 +10,56 @@ var Model = function(modelType) {
 
 // Prototype Inheritance
 
-var gizzmo = new Animal("Gizzmo", "Ned");
-console.log(gizzmo);
+function Animal(name, owner){
+  this.name = name;
+  this.owner = owner;
+}
+
+Animal.prototype.sayOwner = function() {
+  console.log(this);
+  console.log(this.owner);
+}
+
+function Cat(name, owner, furType){
+  Animal.call(this, name, owner);
+
+  this.furType = furType;
+}
+
+function Surrogate(){};
+Surrogate.prototype = new Animal();
+
+Cat.prototype = new Surrogate();
+
+Cat.prototype.meow = function() {
+  console.log("Meow");
+}
+
+Cat.prototype.sayFurType = function() {
+  console.log("My fur type is: " + this.furType);
+}
+
+var gizzmo = new Cat("Gizzmo", "Ned", "Fuzzy");
+gizzmo.meow();
+gizzmo.sayOwner();
+gizzmo.sayFurType();
+
+// Exercises
+
+Function.prototype.inherits = function (baseClass) {
+  function Surrogate() {};
+  Surrogate.prototype = baseClass.prototype;
+  this.prototype = new Surrogate();
+  this.prototype.constructor = this;
+}
+
+function MovingObject (objectType) {
+};
+
+function Ship (objectType) {
+};
+Ship.inherits(MovingObject);
+
+function Asteroid () {
+};
+Asteroid.inherits(MovingObject);
